ebc1f05a91678c0503ab3e00dc78789d4ce1540c
Joel Chi <abongsjoel@gmail.com>
2026-01-02 22:24:22 +0100

    Display Hamburger Icon for mobile

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <menu>
-      <ul className="flex gap-4">
+    <nav>
+      {/* Hamburger Button - visible on mobile, hidden on desktop */}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="md:hidden flex flex-col gap-1 p-2"
+        aria-label="Toggle menu"
+      >
+        <span className="w-6 h-0.5 bg-black"></span>
+        <span className="w-6 h-0.5 bg-black"></span>
+        <span className="w-6 h-0.5 bg-black"></span>
+      </button>
+
+      {/* Menu - hidden on mobile unless open, always visible on desktop */}
+      <ul
+        className={`${
+          isOpen ? "flex" : "hidden"
+        } md:flex flex-col md:flex-row gap-4 absolute md:relative top-full left-0 w-full md:w-auto bg-white md:bg-transparent p-4 md:p-0`}
+      >
         <li>
           <a href="/" className="border-b-2 border-black pb-1 font-semibold">
             Home
@@ -32,7 +52,7 @@ const Menu = () => {
           </a>
         </li>
       </ul>
-    </menu>
+    </nav>
   );
 };
 
