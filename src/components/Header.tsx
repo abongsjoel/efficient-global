1465a4089a3dd22ba2974fa98be4487189491988
Joel Chi <abongsjoel@gmail.com>
2026-01-02 22:20:52 +0100

    Create Standalone menu component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,10 @@
+import Menu from "./Menu";
+
 const Header = () => {
   return (
     <header className="bg-white text-black p-8 flex items-center justify-between">
       Logo
-      <menu>
-        <ul className="flex gap-4">
-          <li>
-            <a href="/" className="border-b-2 border-black pb-1 font-semibold">
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="/services"
-              className="hover:border-b-2 hover:border-black pb-1"
-            >
-              Services
-            </a>
-          </li>
-        </ul>
-      </menu>
+      <Menu />
     </header>
   );
 };
