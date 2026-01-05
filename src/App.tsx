32d60b1c4341b78f8f12dfbb9dcdc66c1a612283
Joel Chi <abongsjoel@gmail.com>
2026-01-05 22:48:23 +0100

    Add Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import Header from "./components/Header";
-import city from "./assets/city.jpg";
+import Hero from "./components/molecules/Hero";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <article
-          className="h-screen bg-cover bg-center flex items-center justify-center"
-          style={{ backgroundImage: `url(${city})` }}
-        >
-          <h1 className="text-white text-5xl font-bold text-center px-4">
-            Welcome to Our City
-          </h1>
-        </article>
+        <Hero />
         <article className="bg-white text-black">More content here...</article>
       </main>
     </div>
