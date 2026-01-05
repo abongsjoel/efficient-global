9ba0938bc0eb69c2b280f51f0e6c2b4532807822
Joel Chi <abongsjoel@gmail.com>
2026-01-05 23:37:10 +0100

    Make fonts proportionately smaller on mobile screens

--- src/components/molecules/Hero.tsx
+++ src/components/molecules/Hero.tsx
@@ -1,4 +1,4 @@
-import city from "../../assets/city.jpg";
+import city from "../../assets/images/city.jpg";
 import Button from "../atoms/Button";
 
 const Hero = () => {
@@ -9,13 +9,13 @@ const Hero = () => {
     >
       <div className="absolute inset-0 bg-black/50"></div>
       <div className="flex flex-col gap-2 items-center relative z-10">
-        <h1 className="text-primary-200 text-6xl font-bold text-center px-4 uppercase font-saira">
+        <h1 className="text-primary-200 text-3xl md:text-6xl font-bold text-center px-4 uppercase font-saira">
           Efficient Global
         </h1>
-        <h2 className="text-white text-3xl">
+        <h2 className="text-white text-xl md:text-3xl text-center px-4">
           Delivering Your Products Fast and Securely
         </h2>
-        <h3>
+        <h3 className="text-white text-sm md:text-base text-center px-4">
           Rapid Delivery Services Serving Maryland, DC, and Northern Virginia
         </h3>
         <Button className="mt-6">GET A QUOTE</Button>
