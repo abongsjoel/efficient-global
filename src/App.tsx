aa97c589a08d8ebb82e57fba794f0f0eff99ea4f
Joel Chi <abongsjoel@gmail.com>
2026-01-05 22:33:10 +0100

    Add Main image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import Header from "./components/Header";
+import city from "./assets/city.jpg";
 
 function App() {
   return (
     <div>
       <Header />
-      <menu>Hellooo</menu>
+      <main>
+        <article
+          className="h-screen bg-cover bg-center flex items-center justify-center"
+          style={{ backgroundImage: `url(${city})` }}
+        >
+          <h1 className="text-white text-5xl font-bold text-center px-4">
+            Welcome to Our City
+          </h1>
+        </article>
+        <article className="bg-white text-black">More content here...</article>
+      </main>
     </div>
   );
 }
