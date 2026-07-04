6e1c189774c981b0a5a60a3825729275ab45014e
Joel Chi <abongsjoel@gmail.com>
2026-07-04 17:45:25 +0100

    Adjust submit button

--- src/components/organisms/DeliveryRequestForm.tsx
+++ src/components/organisms/DeliveryRequestForm.tsx
@@ -158,7 +158,7 @@ const DeliveryRequestForm = () => {
 
         <button
           type="submit"
-          className="inline-flex w-full justify-center rounded-full bg-primary-200 px-8 py-4 text-sm font-semibold uppercase tracking-[0.24em] text-slate-950 transition duration-200 hover:bg-primary-300"
+          className="inline-flex w-full justify-center rounded-full bg-primary-100 px-8 py-4 text-sm font-semibold uppercase tracking-[0.24em] text-slate-950 transition duration-200 hover:bg-primary-200 hover:text-white"
         >
           Submit Request
         </button>
